5c7dbdeca97638a24f1e543a05ce04099e87352f
soeren1907 <sschae55@smail.uni-koeln.de>
2025-06-02 10:18:15 +0200

    i

---
+++ src/main/java/org/example/wkdp2025/Wkdp2025Application.java
@@ -0,0 +1,13 @@
+package org.example.wkdp2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wkdp2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wkdp2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wkdp2025/Wkdp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wkdp2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wkdp2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
